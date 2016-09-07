b370bc5336e2874b815d0cdb131e6a16e3ec4fdc
martin naughton <martin.oneachtain@gmail.com>
2016-09-07 13:23:32 +0100

    2 Functions working on their own.

---
+++ src/2Functions.hs
@@ -0,0 +1,9 @@
+-- This is working. It is taking a tuple as an arguement in both functions.
+
+addBark :: (String, String) -> (String)
+addBark (str, str2) = str ++ str2 ++ " Bark Bark"
+addNumber :: (Int, Int) -> (Int)
+addNumber (a, b) = a + b
+
+main = putStrLn( addBark ("Some ","Dog says: "))
+--main = print (addNumber (1,2))
